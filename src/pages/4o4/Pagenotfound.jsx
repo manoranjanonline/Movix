aa2334986099ed865a3f836c7232dbdcc21e4eaf
aman <lmanoranjan8@gmail.com>
2023-12-13 16:27:15 +0530

    aman

---
+++ src/pages/4o4/Pagenotfound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Style.scss'
+const Pagenotfound = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Pagenotfound
---
+++ src/pages/searchresult/SearchResult.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Style.scss'
+export default function SearchResult() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
