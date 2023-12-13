cb7ad330408e09ea5df780f256bb1622ac8b85d6
Manoranjan Lenka <121720863+manoranjanonline@users.noreply.github.com>
2023-12-13 17:39:09 +0530

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Style.scss'
-import HeroBanner from "./heroBanner/HeroBanner";
+import HeroBanner from "./herobanner/HeroBanner";
 import Trending from "./trending/Trending";
 import Popular from "./popular/Popular";
 import TopRated from "./topRated/TopRated";
