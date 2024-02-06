61ed194f0eb51788c2883ed4f77ebb741b59d4d9
Manoranjan Lenka <121720863+manoranjanonline@users.noreply.github.com>
2024-02-06 11:54:13 +0530

    Update style name

--- src/pages/searchresult/SearchResult.jsx
+++ src/pages/searchresult/SearchResult.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import InfiniteScroll from "react-infinite-scroll-component";
 
-import "./style.scss";
+import "./Style.scss";
 
 import { fetchDataFromApi } from "../../utils/api";
 import ContentWrapper from "../../componets/contentWrapper/ContentWrapper";
